00fc214871348e51a15d8fcc05fbdfe51f6b87ff
Habibur Rahman Masum <hrmasum99.study@gmail.com>
2025-12-07 23:34:07 +0600

    frontend update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const request = async (endpoint, options = {}) => {
   const token = localStorage.getItem('token');
@@ -57,3 +57,5 @@ const api = {
 };
 
 export default api;
+
+console.log(process.env.REACT_APP_API_BASE_URL);
